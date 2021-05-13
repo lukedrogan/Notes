95858295e641db5b4d789d508b9670cb61e95075
Luke Drogan <luke@super-wide.com>
2021-05-13 18:42:35 +0100

    Move files around

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const fs = require('fs');
+
 
 const app = express()
 const PORT = process.env.PORT || 3080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// require('./routes/apiRoutes')(app);
-// require('./routes/htmlRoutes')(app);
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
   });
-  
-  app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
-  
+
