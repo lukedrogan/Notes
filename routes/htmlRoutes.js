311762731a9641c453ad85446dc7b3f10b920fd5
Luke Drogan <luke@super-wide.com>
2021-05-13 18:50:47 +0100

    Added html routing

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const router = require('express').Router();
+
+router.get('/notes', (request, response) => {
+    response.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+router.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3080;
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
